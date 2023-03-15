0416672ab914875fb1a064b8b0986cff19b6b3e6
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-15 16:05:54 +0100

    Add parents stage

--- d_sep_test/src/main.rs
+++ d_sep_test/src/main.rs
@@ -1,3 +1,19 @@
+use d_separation::{add_parents, Graph};
+
 fn main() {
-    println!("Hello, world!");
+    let mut base_graph = Graph::new();
+    base_graph.add_node("A".to_string());
+    base_graph.add_node("B".to_string());
+    base_graph.add_node("C".to_string());
+
+    base_graph.add_edge(&"A".to_string(), &"B".to_string());
+    base_graph.add_edge(&"A".to_string(), &"C".to_string());
+
+    let mut d_sep_graph = Graph::new();
+    d_sep_graph.add_node("B".to_string());
+    d_sep_graph.add_node("C".to_string());
+
+    add_parents(&mut d_sep_graph, &base_graph);
+
+    println!("{:#?}", d_sep_graph);
 }
--- d_separation/src/lib.rs
+++ d_separation/src/lib.rs
@@ -1,31 +1,62 @@
-use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
-struct Node {
+#[derive(Debug)]
+pub struct Node {
     name: String,
-    children: Vec<RefCell<Node>>,
+    edges: Vec<String>,
 }
 
-struct Graph {
-    nodes: Vec<Cell<Node>>,
+#[derive(Debug)]
+pub struct Graph {
+    nodes: HashMap<String, Node>,
 }
 
 impl Graph {
-    fn new() -> Self {
-        Graph { nodes: vec![] }
+    pub fn new() -> Graph {
+        Graph {
+            nodes: HashMap::new(),
+        }
     }
 
-    fn add_node(&mut self, name: &str) -> RefCell<Node> {
+    pub fn add_node(&mut self, name: String) -> String {
         let node = Node {
-            name: name.to_string(),
-            children: vec![],
+            name: name.clone(),
+            edges: Vec::new(),
         };
-        self.nodes.push(Cell::new(node.clone()));
-        RefCell::new(node)
+        self.nodes.insert(name.clone(), node);
+        name
     }
 
-    fn add_edge(&mut self, parent: &RefCell<Node>, child: &RefCell<Node>) {
-        parent.borrow_mut().children.push(child.clone());
+    pub fn add_edge(&mut self, from: &String, to: &String) {
+        let from_node = self.nodes.get_mut(from).unwrap();
+        from_node.edges.push(to.clone());
+    }
+
+    pub fn get_children(&self, node_index: &String) -> Vec<String> {
+        let node = self.nodes.get(node_index).unwrap();
+        node.edges.clone()
+    }
+}
+
+pub fn add_parents(d_sep_graph: &mut Graph, base_graph: &Graph) {
+    let mut changed = true;
+    while changed {
+        changed = false;
+        for possible_parent in base_graph.nodes.keys() {
+            let possible_parent_children = base_graph.get_children(possible_parent);
+            let d_sep_node_keys: Vec<String> = d_sep_graph.nodes.keys().cloned().collect();
+
+            for node in d_sep_node_keys.iter() {
+                if possible_parent_children.contains(node) {
+                    if !d_sep_graph.nodes.contains_key(possible_parent) {
+                        d_sep_graph.add_node(possible_parent.clone());
+                    }
+                    if !d_sep_graph.nodes[possible_parent].edges.contains(node) {
+                        d_sep_graph.add_edge(possible_parent, node);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -34,7 +65,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn add_parents_test() {
+        let mut base_graph = Graph::new();
+        base_graph.add_node("A".to_string());
+        base_graph.add_node("B".to_string());
+        base_graph.add_node("C".to_string());
+
+        base_graph.add_edge(&"A".to_string(), &"B".to_string());
+        base_graph.add_edge(&"A".to_string(), &"C".to_string());
+
+        let mut d_sep_graph = Graph::new();
+        d_sep_graph.add_node("B".to_string());
+        d_sep_graph.add_node("C".to_string());
+
+        add_parents(&mut d_sep_graph, &base_graph);
+
+        assert_eq!(d_sep_graph.nodes.len(), 3);
     }
 }
